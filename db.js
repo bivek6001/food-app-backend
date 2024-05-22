c1b339a5f558cda48cf7abb8d566770abfa13abe
Bivek Singha <134501670+bivek6001@users.noreply.github.com>
2024-05-22 16:36:46 +0530

    added database

--- db.js
+++ db.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const connect= async()=>{
 
     try{
-        await mongoose.connect("mongodb://localhost:27017/foodapp");
+        await mongoose.connect("mongodb+srv://bivekwangkhem6001:6001971918@cluster0.d7rwkcp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
         console.log("connected");
 
     }catch(e){
